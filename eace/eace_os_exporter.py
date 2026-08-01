e457fa105b2184cd3fce5ad2b15cfcefca7a1157
Silas Teixeira <silas.teixeira@bitinternet.com.br>
2026-08-01 15:48:33 -0300

    fix: adicionar import de Keys em eace_os_exporter.py

--- eace/eace_os_exporter.py
+++ eace/eace_os_exporter.py
@@ -23,6 +23,7 @@ import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
